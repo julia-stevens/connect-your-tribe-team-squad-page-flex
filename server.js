390886b33f75c48b16f9fd9f334779e75005e8b3
Matthijs217 <matthijstenbrink@gmail.com>
2025-02-24 16:45:32 +0100

    studenten cards af #3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express'
 import { Liquid } from 'liquidjs';
 
 // Vul hier jullie team naam in
-const teamName = '';
+const teamName = 'Flex';
 
 
 const app = express()
@@ -17,6 +17,10 @@ app.set('views', './views')
 
 app.use(express.urlencoded({extended: true}))
 
+const squadResponse = await fetch('https://fdnd.directus.app/items/squad?filter={"_and":[{"cohort":"2425"},{"tribe":{"name":"FDND Jaar 1"}}]}')
+
+const squadResponseJSON = await squadResponse.json()
+
 app.get('/', async function (request, response) {
   const messagesResponse = await fetch(`https://fdnd.directus.app/items/messages/?filter={"for":"Team ${teamName}"}`)
   const messagesResponseJSON = await messagesResponse.json()
@@ -25,6 +29,25 @@ app.get('/', async function (request, response) {
     teamName: teamName,
     messages: messagesResponseJSON.data
   })
+})
+
+app.get('/studenten', async function (request, response) {
+  let personResponseJSON
+
+  let personURL = await fetch('https://fdnd.directus.app/items/person/?fields=*,squads.squad_id.name,squads.squad_id.cohort&filter=%7B%22_and%22:%5B%7B%22squads%22:%7B%22squad_id%22:%7B%22tribe%22:%7B%22name%22:%22FDND%20Jaar%201%22%7D%7D%7D%7D,%7B%22squads%22:%7B%22squad_id%22:%7B%22cohort%22:%222425%22%7D%7D%7D%5D%7D&sort=name')
+
+  if (request.query.sort == 'za'){
+    const personResponse = await fetch('https://fdnd.directus.app/items/person/?fields=*,squads.squad_id.name,squads.squad_id.cohort&filter=%7B%22_and%22:%5B%7B%22squads%22:%7B%22squad_id%22:%7B%22tribe%22:%7B%22name%22:%22FDND%20Jaar%201%22%7D%7D%7D%7D,%7B%22squads%22:%7B%22squad_id%22:%7B%22cohort%22:%222425%22%7D%7D%7D%5D%7D&sort=-name')
+    personResponseJSON = await personResponse.json()
+  } else if (request.query.sort == 'birthdate'){
+    const personResponse = await fetch('https://fdnd.directus.app/items/person/?fields=*,squads.squad_id.name,squads.squad_id.cohort&filter=%7B%22_and%22:%5B%7B%22squads%22:%7B%22squad_id%22:%7B%22tribe%22:%7B%22name%22:%22FDND%20Jaar%201%22%7D%7D%7D%7D,%7B%22squads%22:%7B%22squad_id%22:%7B%22cohort%22:%222425%22%7D%7D%7D%5D%7D&sort=birthdate')
+    personResponseJSON = await personResponse.json()
+  } else {
+    const personResponse = await fetch('https://fdnd.directus.app/items/person/?fields=*,squads.squad_id.name,squads.squad_id.cohort&filter=%7B%22_and%22:%5B%7B%22squads%22:%7B%22squad_id%22:%7B%22tribe%22:%7B%22name%22:%22FDND%20Jaar%201%22%7D%7D%7D%7D,%7B%22squads%22:%7B%22squad_id%22:%7B%22cohort%22:%222425%22%7D%7D%7D%5D%7D&sort=name')
+    personResponseJSON = await personResponse.json()
+  }
+
+  response.render('studenten.liquid', {persons: personResponseJSON.data, squads: squadResponseJSON.data})
 })
 
 app.post('/', async function (request, response) {
