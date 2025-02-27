385dca55073a7bb6277cc91137767872c578da9f
Matthijs217 <matthijstenbrink@gmail.com>
2025-02-27 10:57:37 +0100

    variable voor de refreshknop gemaakt #3
    Als je nu op de refresh knop klikt krijg je een ander puntje informatie te zien van de persoon

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,3 +8,26 @@ inputOutputs.forEach(inputOutput => {
         output.value = input.value
     })
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".refresh-btn").forEach(btn => {
+      btn.addEventListener("click", function () {
+        let li = this.closest("li");
+        let infoText = li.querySelector(".info-text");
+  
+        let dataOptions = [
+          { key: "birthdate", label: "Geboortedatum" },
+          { key: "fav_color", label: "Favoriete kleur" },
+          { key: "most_energy", label: "Meeste energie in de" },
+        ];
+
+
+  
+        let randomOption = dataOptions[Math.floor(Math.random() * dataOptions.length)];
+        let newValue = li.dataset[randomOption.key];
+  
+        infoText.textContent = `${randomOption.label}: ${newValue}`;
+      });
+    });
+  });
+  
